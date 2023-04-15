8b3b3e4b984a3f8df73d9e07183de55070663bbf
Stefan Samson <59541034+SSbit01@users.noreply.github.com>
2023-04-15 14:28:24 +0200

    refactor: remove compress

--- astro.config.ts
+++ astro.config.ts
@@ -1,13 +1,9 @@
 import { defineConfig } from "astro/config"
 
-import compress from "astro-compress"
-
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    compress()
-  ],
+  integrations: [],
   
   site: "https://ssbit01.github.io/"
 })
