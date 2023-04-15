de2c68fddd7f8b59eba1efb2f594ef3008cd4dab
Stefan Samson <59541034+SSbit01@users.noreply.github.com>
2023-04-15 12:56:37 +0200

    refactor: remove sitemap, it's useless

--- astro.config.ts
+++ astro.config.ts
@@ -1,13 +1,11 @@
 import { defineConfig } from "astro/config"
 
-import sitemap from "@astrojs/sitemap"
 import compress from "astro-compress"
 
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [
-    sitemap(),
     compress()
   ],
   
