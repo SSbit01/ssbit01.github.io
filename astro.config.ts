8bb7b199b66dee90c64d85a4ae2824571a0fef41
Stefan Samson <59541034+SSbit01@users.noreply.github.com>
2023-04-22 19:53:38 +0200

    chore: astro-compress returns (1.1.32 minifies HTML successfully)

--- astro.config.ts
+++ astro.config.ts
@@ -1,9 +1,15 @@
 import { defineConfig } from "astro/config"
 
+import compress from "astro-compress"
+
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [],
   
+  integrations: [
+    compress()
+  ],
+
   site: "https://ssbit01.github.io/"
+
 })
