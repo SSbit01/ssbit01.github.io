5b817a598d2147df030d8c12892024e817560fb3
Stefan Samson <ss42701@outlook.com>
2026-05-21 16:47:41 +0200

    refactor

--- src/data/social.ts
+++ src/data/social.ts
@@ -10,28 +10,10 @@ export const github: Social = {
   icon: "line-md:github-twotone"
 };
 
-export const replit: Social = {
-  href: "https://replit.com/@SSbit01",
-  title: "Replit",
-  icon: "simple-icons:replit"
-};
-
 export const codepen: Social = {
   href: "https://codepen.io/ssbit01",
   title: "CodePen",
   icon: "teenyicons:codepen-solid"
-};
-
-export const freecododecamp: Social = {
-  href: "https://www.freecodecamp.org/ssbit01",
-  title: "freeCodeCamp",
-  icon: "simple-icons:freecodecamp"
-};
-
-export const linkedin: Social = {
-  href: "https://www.linkedin.com/in/ssbit01/",
-  title: "LinkedIn",
-  icon: "ri:linkedin-fill"
 };
 
 export const email: Social = {
